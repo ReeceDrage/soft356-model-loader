7304b243fa86688a1cf3518f9096a368bf5c3d59
ReeceStudent <reece.drage@students.plymouth.ac.uk>
2019-10-15 20:27:57 +0100

    Added basic nupengl code
    Added code to initialise a rendering window, poll for events and buffer swapping.

--- Model Loader/ModelLoader/ModelLoader/ModelLoader.cpp
+++ Model Loader/ModelLoader/ModelLoader/ModelLoader.cpp
@@ -1,3 +1,22 @@
 // Standard C/C++ include statements
 #include <iostream>
 #include <cstdlib>
+
+// Nupengl include statements for OpenGL rendering
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+#include "GLFW/glfw3.h"
+
+int main(int argc, char** argv)
+{
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "Render Window", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glewInit();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+}
