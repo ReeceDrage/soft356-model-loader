31bdb3e03a6b154f8084b86247635eb29f5ad252
ReeceStudent <reece.drage@students.plymouth.ac.uk>
2019-10-15 22:40:46 +0100

    Implemented file loading
    Modified ModelLoader.cpp to include basic file loading of an obj file into a vector structure.

--- Model Loader/ModelLoader/ModelLoader/ModelLoader.cpp
+++ Model Loader/ModelLoader/ModelLoader/ModelLoader.cpp
@@ -1,6 +1,11 @@
 // Standard C/C++ include statements
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
 
 // Nupengl include statements for OpenGL rendering
 #include "GL/glew.h"
@@ -9,6 +14,21 @@
 
 int main(int argc, char** argv)
 {
+	// Create input stream
+	string line;
+	vector<string> mapData;
+	ifstream myFile("Resources/suzanne.obj");
+
+	if (myFile.is_open())
+	{
+		while (getline(myFile, line))
+		{
+			mapData.push_back(line);
+		}
+
+		myFile.close();
+	}
+
 	glfwInit();
 	GLFWwindow* window = glfwCreateWindow(1920, 1080, "Render Window", NULL, NULL);
 	glfwMakeContextCurrent(window);
