be486cb772b24c8fb0480cf61a900020bc0f048e
ReeceStudent <47244831+ReeceStudent@users.noreply.github.com>
2019-11-01 19:40:43 +0000

    Added definitions for GetErrorString and AddToErrorString
    Added definitions for the GetErrorString and AddToErrorString functions. Added the private global variable errorMessage. This will be used for returning collated error messages after parsing fails.

--- Model Viewer/Model Viewer/Model Parser.h
+++ Model Viewer/Model Viewer/Model Parser.h
@@ -7,11 +7,16 @@ class OBJParser
 {
 public:
 	bool ParseOBJ(std::vector<std::string> rawData, Model *model);
+	void GetErrorString(std::vector<std::string>* errorVector);
 
 private:
+	std::vector<std::string> errorMessage;
+
 	bool StringSplit(std::string data, std::vector<std::string>* splitData, char delimiter, bool removeWhitespace);
 	bool ParseVertexCoordinates(std::vector<std::string> data, glm::vec4* vertex);
 	bool ParseFaceCoordinates(std::vector<std::string> data, FaceRecord* face);
 	bool ParseTextureCoordinates(std::vector<std::string> data, glm::vec2* texture);
 	bool ParseNormalCoordinates(std::vector<std::string> data, glm::vec4* normal);
+
+	void AddToErrorString(std::string error);
 };
