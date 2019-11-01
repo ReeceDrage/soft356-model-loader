e731a9b7200009b3d51a2666decbaded663494e0
ReeceStudent <47244831+ReeceStudent@users.noreply.github.com>
2019-11-01 18:04:57 +0000

    Modified Model Parser
    Modified StringSplit definition to add  a pointer to a vector for returning data, as well as changing a string delimiter to a char type.

--- Model Viewer/Model Viewer/Model Parser.h
+++ Model Viewer/Model Viewer/Model Parser.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ModelStructs.h"
+#include <vector>
 
 class OBJParser
 {
@@ -8,7 +9,7 @@ public:
 	bool ParseOBJ(std::vector<std::string> rawData, Model model);
 
 private:
-	bool StringSplit(std::string data, std::string delimiter);
+	bool StringSplit(std::string data, std::vector<std::string>* splitData, char delimiter);
 	bool ParseVertexCoordinates(std::string data, glm::vec4 vertex);
 	bool ParseFaceCoordinates(std::string data, FaceRecord face);
 	bool ParseTextureCoordinates(std::string data, glm::vec2 texture);
