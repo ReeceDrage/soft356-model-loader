bed305595b1a6d9b0b2c0cc0d5757934c62c2058
ReeceStudent <47244831+ReeceStudent@users.noreply.github.com>
2019-11-02 14:20:24 +0000

    Implemented ParseOBJ and ParseObject
    Full implementation of ParseOBJ and ParseObject completed. OBJ parser should now be functional, though polish is needed.

--- Model Viewer/Model Viewer/Model Parser.cpp
+++ Model Viewer/Model Viewer/Model Parser.cpp
@@ -19,7 +19,211 @@ using std::stringstream;
 
 bool OBJParser::ParseOBJ(vector<string> rawData, Model *model)
 {
+	vector<vector<string>> data;
+	int numberOfObjects = 0;
+
+	for (int i = 0; i < rawData.size(); i++)
+	{
+		// Split each string for easy access to the identifying prefix
+		vector<string> splitString;
+		StringSplit(rawData[i], &splitString, ' ', true);
+
+		// If an object is found
+		if (splitString[0] == "o")
+		{
+			// Ignore the initial comments
+			if (numberOfObjects > 0)
+			{
+				ModelObject object;
+				ParseObject(&data, &object);
+				model->objects.push_back(object);
+				data.clear();
+			}
+
+			numberOfObjects++;
+		}
+		else
+		{
+			data.push_back(splitString);
+		}
+	}
+
+	// Ensure the final object is parsed and stored, or the full structure if no objects were detected
+	ModelObject object;
+	ParseObject(&data, &object);
+	model->objects.push_back(object);
+	data.clear();
+
 	return true;
+}
+
+bool OBJParser::ParseObject(const vector<vector<string>>* data, ModelObject* object)
+{
+	// Variables to store coordinate values
+	vector<vec4> tempVectorCoordinates;
+	vector<vec2> tempTextureCoordinates;
+	vector<vec4> tempNormalCoordinates;
+
+	// Variables to handle faces and materials
+	vector<string> materialNames;
+	vector<vector<string>> materialsAndFaces;
+	vector<FaceRecord> faceRecords;
+	vector<vector<FaceRecord>> vertexGroupFaces;
+
+	bool parseSuccessful = true;
+	bool materialPresent = false;
+
+	for (int i = 0; i < data->size(); i++)
+	{
+		// Retrieve vertex, texture and normal data, then parse it into buffers for processing
+		if ((*data)[i][0] == "v")
+		{
+			vec4 vertex;
+			parseSuccessful = ParseVertexCoordinates((*data)[i], &vertex);
+			tempVectorCoordinates.push_back(vertex);
+		}
+		else if ((*data)[i][0] == "vt")
+		{
+			vec2 texture;
+			parseSuccessful = ParseTextureCoordinates((*data)[i], &texture);
+			tempTextureCoordinates.push_back(texture);
+		}
+		else if ((*data)[i][0] == "vn")
+		{
+			vec4 normal;
+			parseSuccessful = ParseNormalCoordinates((*data)[i], &normal);
+			tempNormalCoordinates.push_back(normal);
+		}
+
+		// Place material and face data into a temporary storage for parsing
+		else if ((*data)[i][0] == "usemtl")
+		{
+			materialPresent = true;
+			materialsAndFaces.push_back((*data)[i]);
+		}
+		else if ((*data)[i][0] == "f")
+		{
+			materialsAndFaces.push_back((*data)[i]);
+		}
+	}
+
+	// If material data is used, parse into groups, otherwise parse as a single group
+	if (materialPresent)
+	{
+		int startingIndex = 0;
+
+		// Store the ID name of the first material
+		materialNames.push_back(materialsAndFaces[0][1]);
+
+		for (int i = 1; i < materialsAndFaces.size(); i++)
+		{
+			if (materialsAndFaces[i][0] == "usemtl")
+			{
+				// Store the ID name of the material
+				materialNames.push_back(materialsAndFaces[i][1]);
+
+				// Parse between the last two usemtl lines identified
+				for (int j = startingIndex + 1; j < i; j++)
+				{
+					// Retrieve the line, parse and add it to the vector
+					FaceRecord face;
+					parseSuccessful = ParseFaceCoordinates(materialsAndFaces[j], &face);
+					faceRecords.push_back(face);
+					face.vertexVector.clear();
+				}
+
+				// Add the vector to the vertexGroup vector
+				startingIndex = i;
+				vertexGroupFaces.push_back(faceRecords);
+				faceRecords.clear();
+			}
+
+			// Ensure the last group is also parsed correctly
+			else if (i == materialsAndFaces.size() - 1)
+			{
+				for (int j = startingIndex + 1; j <= i; j++)
+				{
+					// Retrieve the line, parse and add it to the vector
+					FaceRecord face;
+					parseSuccessful = ParseFaceCoordinates(materialsAndFaces[j], &face);
+					faceRecords.push_back(face);
+					face.vertexVector.clear();
+				}
+
+				// Add the vector to the vertexGroup vector
+				vertexGroupFaces.push_back(faceRecords);
+				faceRecords.clear();
+			}
+		}
+	}
+	else
+	{
+		FaceRecord face;
+
+		for (int i = 0; i < materialsAndFaces.size(); i++)
+		{
+			// Retrieve the line, parse and add it to the vector
+			FaceRecord face;
+			parseSuccessful = ParseFaceCoordinates(materialsAndFaces[i], &face);
+			faceRecords.push_back(face);
+		}
+
+		vertexGroupFaces.push_back(faceRecords);
+	}
+
+	// Process every face record to retrieve vertex, texture and normal indices
+	for (int i = 0; i < vertexGroupFaces.size(); i ++)
+	{
+		VertexGroup vertexGroup;
+
+		for (int j = 0; j < vertexGroupFaces[i].size(); j++)
+		{
+			FaceRecord face = vertexGroupFaces[i][j];
+
+			// If quad data is found, split into a pair of triangles
+			if (face.vertexVector.size() == 4)
+			{
+				// Triangle one
+				vertexGroup.vertexCoordinates.push_back(tempVectorCoordinates[face.vertexVector[0].vertexIndex - 1]);
+				vertexGroup.textureCoordinates.push_back(tempTextureCoordinates[face.vertexVector[0].textureIndex - 1]);
+				vertexGroup.normalCoordinates.push_back(tempNormalCoordinates[face.vertexVector[0].normalIndex - 1]);
+
+				vertexGroup.vertexCoordinates.push_back(tempVectorCoordinates[face.vertexVector[1].vertexIndex - 1]);
+				vertexGroup.textureCoordinates.push_back(tempTextureCoordinates[face.vertexVector[1].textureIndex - 1]);
+				vertexGroup.normalCoordinates.push_back(tempNormalCoordinates[face.vertexVector[1].normalIndex - 1]);
+
+				vertexGroup.vertexCoordinates.push_back(tempVectorCoordinates[face.vertexVector[2].vertexIndex - 1]);
+				vertexGroup.textureCoordinates.push_back(tempTextureCoordinates[face.vertexVector[2].textureIndex - 1]);
+				vertexGroup.normalCoordinates.push_back(tempNormalCoordinates[face.vertexVector[2].normalIndex - 1]);
+
+				// Triangle two
+				vertexGroup.vertexCoordinates.push_back(tempVectorCoordinates[face.vertexVector[2].vertexIndex - 1]);
+				vertexGroup.textureCoordinates.push_back(tempTextureCoordinates[face.vertexVector[2].textureIndex - 1]);
+				vertexGroup.normalCoordinates.push_back(tempNormalCoordinates[face.vertexVector[2].normalIndex - 1]);
+
+				vertexGroup.vertexCoordinates.push_back(tempVectorCoordinates[face.vertexVector[3].vertexIndex - 1]);
+				vertexGroup.textureCoordinates.push_back(tempTextureCoordinates[face.vertexVector[3].textureIndex - 1]);
+				vertexGroup.normalCoordinates.push_back(tempNormalCoordinates[face.vertexVector[3].normalIndex - 1]);
+
+				vertexGroup.vertexCoordinates.push_back(tempVectorCoordinates[face.vertexVector[0].vertexIndex - 1]);
+				vertexGroup.textureCoordinates.push_back(tempTextureCoordinates[face.vertexVector[0].textureIndex - 1]);
+				vertexGroup.normalCoordinates.push_back(tempNormalCoordinates[face.vertexVector[0].normalIndex - 1]);
+			}
+			else
+			{
+				for (int k = 0; k < vertexGroupFaces[i][j].vertexVector.size(); k++)
+				{
+					vertexGroup.vertexCoordinates.push_back(tempVectorCoordinates[face.vertexVector[k].vertexIndex - 1]);
+					vertexGroup.textureCoordinates.push_back(tempTextureCoordinates[face.vertexVector[k].textureIndex - 1]);
+					vertexGroup.normalCoordinates.push_back(tempNormalCoordinates[face.vertexVector[k].normalIndex - 1]);
+				}
+			}
+		}
+
+		object->vertexGroups.push_back(vertexGroup);
+	}
+
+	return parseSuccessful;
 }
 
 bool OBJParser::StringSplit(string data, vector<string>* splitData, char delimiter, bool removeWhiteSpace)
